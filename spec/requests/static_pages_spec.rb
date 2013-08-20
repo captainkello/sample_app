1c65f544cfcff5d40f1cfd4a68d300ddfa237312
Captain Kello <captainkello@gmail.com>
2013-08-20 14:40:44 -0700

    Fixed routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -23,12 +23,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit 'help_path'
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit 'help_path'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
@@ -36,12 +36,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit 'about_path'
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit 'about_path'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
@@ -49,12 +49,12 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit 'contact_path'
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit 'contact_path'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
