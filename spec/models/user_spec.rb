98f8756f260d1670f0c33e81abd8e1fd40da9e50
Captain Kello <captainkello@gmail.com>
2013-08-21 13:49:55 -0700

    update gemfile for rcrypt

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -75,4 +75,16 @@ describe User do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
+
+  describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+end
 end
